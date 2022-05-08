3d35759ed60eff1e038bfb750a318705a0afb453
aramirezr2 <a.ramirezr2@uniandes.edu.co>
2022-05-08 09:31:37 -0500

    Se separan los steps de login y de members.

--- Kraken/features/web/step_definitions/step_members.js
+++ Kraken/features/web/step_definitions/step_members.js
@@ -1,20 +1,6 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const expect = require('chai').expect;
 
-// Login Steps.
-When('I enter email {kraken-string}', async function (email) {
-    let element = await this.driver.$('#ember7');
-    return await element.setValue(email);
-});
-When('I enter password {kraken-string}', async function (password) {
-    let element = await this.driver.$('#ember9');
-    return await element.setValue(password);
-});
-When('I click signin', async function() {
-    let element = await this.driver.$('#ember11');
-    return await element.click();
-})
-
 // Member Steps 
 When(' s', async function() {
     let element = await this.driver.$('#ember77');
